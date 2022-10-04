d55e7c7445a4993cb8751ff4ce1985c457835439
Mustafa Neto <mustafaneto26@gmail.com>
2022-10-04 14:51:14 -0300

    aprendendo-pinia

---
+++ learn-pinia/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <button class="buttons" @click="storeCounter.increment">{{ storeCounter.counter }}</button>
+    <br>
+    <h3>Type new number: </h3>
+    <input type="number" v-model="storeCounter.counter">
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
+
+<style scoped>
+
+.buttons {
+  margin: 10px;
+  width: auto;
+  font-size: 40px;
+}
+</style>
---
+++ learn-pinia/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div class="counter">{{ storeCounter.counter }}</div>
+    <div>
+      <button class="buttons" @click="storeCounter.decrement">-</button>
+      <button class="buttons" @click="storeCounter.increment">+</button>
+      <h1>The double is: {{storeCounter.doubleCount}}</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
+
+<style scoped>
+.counter {
+  font-size: 40px;
+}
+.buttons {
+  margin: 10px;
+  width: 60px;
+  font-size: 40px;
+}
+</style>
